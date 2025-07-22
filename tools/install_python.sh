dd4c8821b2a27e2ed3c76be31a8b2d8a420895fb
Server <andach2002@hotmail.com>
2025-07-22 21:39:16 +0000

    fixes

--- tools/install_python.sh
+++ tools/install_python.sh
@@ -11,18 +11,18 @@ echo "🔧 Installing system dependencies..."
 sudo apt install python3-pip python3-venv -y
 
 # Create the virtual environment only if it doesn't exist
-if [ ! -d "sources-py/env" ]; then
+if [ ! -d "source/env" ]; then
   echo "📦 Creating new virtual environment..."
-  python3 -m venv sources-py/env
+  python3 -m venv source/env
 else
   echo "✅ Virtual environment already exists."
 fi
 
 # Activate it and install requirements
 echo "📥 Installing Python packages from requirements.txt..."
-source sources-py/env/bin/activate
+source source/env/bin/activate
 pip install --upgrade pip
-pip install -r sources-py/requirements_linux.txt
+pip install -r source/requirements_linux.txt
 deactivate
 
 echo "✅ Python environment setup complete!"
